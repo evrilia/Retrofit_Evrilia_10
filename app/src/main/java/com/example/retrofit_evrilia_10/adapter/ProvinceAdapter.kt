64f4ede1d0afb05aa015c5fc6715f37b72e13122
evrilia <evriliaelsyabilaa@gmail.com>
2022-02-25 13:30:10 +0700

    first commit

---
+++ app/src/main/java/com/example/retrofit_evrilia_10/adapter/ProvinceAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.retrofit_evrilia_10.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.retrofit_evrilia_10.R
+import com.example.retrofit_evrilia_10.model.ProvinceResponse
+import kotlinx.android.synthetic.main.item_province.view.*
+
+class ProvinceAdapter (private var list: ArrayList<ProvinceResponse>): RecyclerView.Adapter<ProvinceAdapter.ProvinceViewHolder>(){
+    inner class ProvinceViewHolder(itemview: View): RecyclerView.ViewHolder(itemview){
+        fun bind(province: ProvinceResponse){
+            with(itemView){
+                tvName.text = province.attributes.province
+                tvPositive.text = province.attributes.positive.toString()
+                tvRecover.text = province.attributes.recover.toString()
+                tvDeath.text = province.attributes.death.toString()
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProvinceViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_province, parent, false)
+        return ProvinceViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ProvinceViewHolder, position: Int) {
+        holder.bind(list[position])
+    }
+
+    override fun getItemCount(): Int = list.size
+}
---
+++ app/src/main/java/com/example/retrofit_evrilia_10/model/ProvinceResponse.kt
@@ -0,0 +1,5 @@
+package com.example.retrofit_evrilia_10.model
+
+data class ProvinceResponse(
+    val attributes: Province
+)
